076f62938e374b0652bc6ac1b8e9c454ff396f98
vad-nes <vad.kachanovich@gmail.com>
2025-03-12 22:57:50 +0300

    fixes

--- src/test/java/ui/HomepageTests.java
+++ src/test/java/ui/HomepageTests.java
@@ -3,17 +3,25 @@ package ui;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 import java.util.List;
+import java.util.Objects;
 
 public class HomepageTests {
 
     WebDriver driver;
+    WebDriverWait waiter;
 
     public static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
     public static final String CHAPTER_LOCATOR = "//div/h5[@class='card-title' and contains(text(),";
@@ -22,33 +30,46 @@ public class HomepageTests {
     @BeforeEach
     void setup() {
         driver = new ChromeDriver();
+        waiter = new WebDriverWait(driver, Duration.ofSeconds(2));
         driver.manage().window().maximize();
         driver.get(BASE_URL);
+
     }
 
     @AfterEach
     void tearDown() {
+        //System.out.println(driver.getPageSource());
         driver.quit();
 
     }
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/testData.csv", numLinesToSkip = 1)
-    void toCheckChaptersElementsTest(String chapterGroup, String chapterElements,String title) {
+    @Test
+    void toCheckChaptersElementsTest() {
 
-        int size = driver.findElements(By.xpath(CHAPTER_LOCATOR + chapterGroup)).size();
+        int elementsCount = 0;
 
-        for (int i = 0; i < size; i++) {
+            List <WebElement> chaptersList = driver.findElements(By.xpath("//div/h5[@class= 'card-title']"));
 
-            List<WebElement> el = driver.findElements(By.xpath(CHAPTER_LOCATOR + chapterGroup));
+            for (var chapter : chaptersList) {
 
-            Assertions.assertTrue(chapterElements.contains(el.get(i).getText()),"ERROR WITH " + el.get(i).getText());
-            el.get(i).click();
+                List <WebElement> elements = chapter.findElements(By.xpath("./ .. /a"));
 
-            Assertions.assertNotEquals(BASE_URL, driver.getCurrentUrl().toString());
-            Assertions.assertEquals(title, driver.getTitle());
+                elementsCount += elements.size();
 
-            driver.get(BASE_URL);
-        }
+                for (var element : elements) {
+
+                    String elementHrefLink = BASE_URL + element.getDomAttribute("href");
+                    System.out.println(element.getText());
+                    element.click();
+                    String elementLink = driver.getCurrentUrl();
+
+                    driver.navigate().back();
+                    Assertions.assertEquals(elementHrefLink, elementLink);
+                }
+
+            }
+
+            Assertions.assertEquals(27, elementsCount);
+
     }
 }
